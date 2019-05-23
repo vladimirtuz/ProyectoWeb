db32c9c4c7e1bc0112d83e1c5793151f482a1e52
vladimirtuz <alextuz97@outlook.com>
2019-05-22 20:46:04 -0500

    Update detalles-proyecto.php

--- src/detalles-proyecto.php
+++ src/detalles-proyecto.php
@@ -3,6 +3,9 @@ session_start();
 require("../db/db.php");
 $conexion=mysqli_connect($servidor,$usuario,$password,$bd);
 $id_proyecto=$_POST['id_proyecto'];
+$sentencia="select *from proyectos where id_proyecto=$id_proyecto";
+$execute=mysqli_query($conexion,$sentencia);
+if($fila=mysqli_fetch_assoc($execute)){
 ?>
 
 
@@ -29,8 +32,22 @@ $id_proyecto=$_POST['id_proyecto'];
              </div>     
        </div>
 
+        <div class="row">
+            <div class="col-md-12" style="text-align:center;">
+              <h1> Informacón del proyecto </h1>
+            </div>
+            <div class="col-md-12"> <h3>Nombre del proyecto: <?php  echo $fila['nombre']; ?> <h3></div>
+            <div class="col-md-6"> <h3>Departamento: <?php  echo $fila['departamento']; ?> <h3></div>
+            <div class="col-md-6"> <h3>Division: <?php  echo $fila['division']; ?> <h3></div>
+            <div class="col-md-6"> <h3>Responsable: <?php  echo $fila['nombre_responsable']; ?> <h3></div>
+            <div class="col-md-6"> <h3>Cargo: <?php  echo $fila['cargo_responsable']; ?> <h3></div>
+            <div class="col-md-12" style="text-align:center"><h3>Objetivo</h3></div>
+            <div class="col-md-12"><p><?php echo $fila["objetivo_general"] ?></p> </div>
+
+
+        </div>
     </body>
 </html>
-
+<?php  }  ?>
 
 
